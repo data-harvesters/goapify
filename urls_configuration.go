10e7e8aa1621b544a1656f3aeacbf23d0dd72837
skateboard <withdraws@gmail.com>
2024-10-11 10:40:04 +0400

    Update urls_configuration.go

--- urls_configuration.go
+++ urls_configuration.go
@@ -4,6 +4,43 @@ type UrlConfigurations []UrlConfiguration
 
 type UrlConfiguration struct {
 	Url string `json:"url"`
+
+	Method   *string         `json:"method"`
+	Payload  *string         `json:"payload"`
+	UserData *map[string]any `json:"userData"`
+	Headers  map[string]any  `json:"headers"`
+}
+
+func (c *UrlConfiguration) GetMethod() string {
+	if c.Method == nil {
+		return ""
+	}
+
+	return *c.Method
+}
+
+func (c *UrlConfiguration) GetPayload() string {
+	if c.Payload == nil {
+		return ""
+	}
+
+	return *c.Payload
+}
+
+func (c *UrlConfiguration) GetUserData() map[string]any {
+	if c.UserData == nil {
+		return nil
+	}
+
+	return *c.UserData
+}
+
+func (c *UrlConfiguration) GetHeaders() map[string]any {
+	if c.Headers == nil {
+		return nil
+	}
+
+	return *&c.Headers
 }
 
 func (c UrlConfigurations) GetUrls() []string {
